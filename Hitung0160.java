079f48aaca1b38664cada5e6938059c7e0155c54
Eka Muliyana <127606389+EkaMuliyana@users.noreply.github.com>
2023-03-30 11:17:34 +0800

    Add files via upload

---
+++ Hitung0160.java
@@ -0,0 +1,17 @@
+/*13020210160*/
+/*EKA MULIYANA*/
+
+import java.util.Scanner;
+
+public class Hitung0001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan mil: ");
+        double mil = input.nextDouble();
+
+        double meter = mil * 1609;
+
+        System.out.println("Hasil " + mil + " mil = " + meter + " meter");
+    }
+}
